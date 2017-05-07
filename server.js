0272f24ecadd9c340d5f7546794007626c6cf9a5
Estefania Cass <TefiC@user.noreply.github.com>
2017-05-07 22:30:51 +0000

    Made initial app, extracted ip address, language and agent from request headers and send JSON to client

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    
+    res.writeHead(200, {'content-type':'application/JSON'});
+    
+    var ipAddress = req.headers["x-forwarded-for"];
+    var language = req.headers["accept-language"].split(',')[0];
+    var userAgent = req.headers["user-agent"];
+
+    var dataJSON = {
+        "ipAddress": ipAddress,
+        "Language": language,
+        "Software": userAgent
+    }
+    
+    console.log(req.headers);
+
+    res.end(JSON.stringify(dataJSON));
+});
 
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port!')
-})
+  console.log('Example app listening on port!');
+});
